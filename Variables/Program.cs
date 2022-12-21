6472ade3245fc912c818587f0ba1e9e1a65cb31c
MaksimMelan <64986385+MaksimMelan@users.noreply.github.com>
2022-12-21 15:50:18 +0500

    Добавьте файлы проекта.

---
+++ Variables/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Principal;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool smoke = false; // дымы для маскировки своего или вражеского юнита. юнит в дыму или нет
+            byte health = 145; // здоровье героя
+            sbyte reputation = -120;
+            short creditAccount = -20000;
+            ushort ammunition = 30000;
+            int coordinatesX = 12313;
+            int coordinatesY = -1242352435;
+            int coordinatesZ = 134;
+            uint distance = 294967295;
+            long ; // незнаю что придумать
+            double; // незнаю что придумать
+            decimal;// незнаю что придумать
+            char yes = 'y';
+            string commandOrder = "Capture the airfield";
+            object radius = 56.579;
+
+        }
+    }
+}
