de0a6dd12b5cddb152ac738690383837965862f1
MaksimMelan <64986385+MaksimMelan@users.noreply.github.com>
2022-12-25 01:28:30 +0500

    name age

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -6,22 +6,34 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            bool smoke = false; // дымы для маскировки своего или вражеского юнита. юнит в дыму или нет
-            byte health = 145; // здоровье героя
-            sbyte reputation = -120;
-            short creditAccount = -20000;
-            ushort ammunition = 30000;
-            int coordinatesX = 12313;
-            int coordinatesY = -1242352435;
-            int coordinatesZ = 134;
-            uint distance = 294967295;
-            ulong grams = 1231; // незнаю что придумать
-            double immunityToBreakdown = - 1.56; // незнаю что придумать
-            decimal theInfluenceOfCivilization =  12.89m;// незнаю что придумать
-            char yes = 'y';
-            string commandOrder = "Capture the airfield";
-            object radius = 56.579;
+            string name;
+            int age;
+            string birthday;
+            string zodiacSign;
+            string sity;
+            string profession;
 
+            Console.WriteLine
+                ("Здравствуйте!");
+            Console.Write("Укажите Ваше имя: ");
+            name = Console.ReadLine();
+            Console.Write("Укажите свой возраст: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Укажите свой день рождения: ");
+            birthday = Console.ReadLine();
+            Console.Write("Укажите свой знак задиака: ");
+            zodiacSign = Console.ReadLine();
+            Console.Write("Укажите место проживания: ");
+            sity = Console.ReadLine();
+            Console.Write("Укажите свою профессию: ");
+            profession = Console.ReadLine();
+
+            Console.WriteLine("");
+            Console.WriteLine($"Вас зовут {name}, возраст {age}, дата вашего рождения {birthday}");
+            Console.WriteLine($"Ваш знак задиака {zodiacSign}, место проживания {sity}, профессия {profession} ");
+           
+            
+            Console.ReadKey();
         }
     }
 }
