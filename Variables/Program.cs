7d4f8e2859961364592553e498cfda7814835242
MaksimMelan <64986385+MaksimMelan@users.noreply.github.com>
2023-02-17 15:49:48 +0500

    if

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -7,33 +7,38 @@ namespace Variables
         static void Main(string[] args)
         {
             string name;
-            int age;
-            string birthday;
-            string zodiacSign;
             string sity;
-            string profession;
 
-            Console.WriteLine
-                ("Здравствуйте!");
-            Console.Write("Укажите Ваше имя: ");
+            Console.WriteLine("Здравствуйте! Как Вас зовут? ");
             name = Console.ReadLine();
-            Console.Write("Укажите свой возраст: ");
-            age = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Укажите свой день рождения: ");
-            birthday = Console.ReadLine();
-            Console.Write("Укажите свой знак задиака: ");
-            zodiacSign = Console.ReadLine();
-            Console.Write("Укажите место проживания: ");
-            sity = Console.ReadLine();
-            Console.Write("Укажите свою профессию: ");
-            profession = Console.ReadLine();
 
-            Console.WriteLine("");
-            Console.WriteLine($"Вас зовут {name}, возраст {age}, дата вашего рождения {birthday}");
-            Console.WriteLine($"Ваш знак задиака {zodiacSign}, место проживания {sity}, профессия {profession} ");
-           
-            
-            Console.ReadKey();
+            if ( name == "Максим")
+            {
+                Console.WriteLine("Здравствуйте " + name + "." + "Пройдите пожалуйста проверку");
+            }
+            else
+            {
+                Console.WriteLine("У Вас нет доступа");
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("В каком городе Вы живёте?");
+            sity = Console.ReadLine();
+
+            if (sity == "Оренбург")
+            {
+                Console.WriteLine("ААА! Оренбургские пуховые платки?...");
+            }
+            else if (sity == "Самара")
+            {
+                Console.WriteLine("AAA!Бункер Сталина?...");
+            }
+            else if (sity == "Москва")
+            {
+                Console.WriteLine("ААА! Золотые купала?...");
+            }
+            Console.WriteLine("Ну проходи гостем будешь");
+            Console.ReadLine();
         }
     }
 }
