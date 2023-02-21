cc81aeccc42f54582a1df34efadbbf60212bb157
MaksimMelan <64986385+MaksimMelan@users.noreply.github.com>
2023-02-21 22:58:24 +0500

    while

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -6,57 +6,13 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            int rubToUSD = 72, usdToRub = 75;
-            float rub;
-            float usd;
-            string userInput;
-            float currencyCount;
+            int age = 10;
 
-            Console.WriteLine("Какую операцию хотите провести?");
-
-            Console.WriteLine("ведите баланс рублей");
-            rub = Convert.ToSingle(Console.ReadLine());
-            Console.WriteLine("введите баланс долларов");
-            usd = Convert.ToSingle(Console.ReadLine());
-
-            
-            Console.WriteLine("1 -  рубли на доллары.");
-            Console.WriteLine("2 - доллары на рубли.");            
-            userInput = Console.ReadLine();
-            
-            switch (userInput)
+            while(age > 0)
             {
-                case "1":
-                    Console.WriteLine("1 - обмен  рубли на доллары.");
-                    Console.WriteLine("Сколько вы хотите обменять?");
-                    currencyCount = Convert.ToSingle(Console.ReadLine());
-                    
-                    if (rub >= currencyCount)
-                    {
-                        rub -= currencyCount;
-                        usd += currencyCount / rubToUSD; 
-                    }
-                    else
-                    {
-                        Console.WriteLine("Недопустимое количесвто рублей");
-                    }
-                    break;
-                case "2":
-                    Console.WriteLine("2 - доллары на рубли."); 
-                    Console.WriteLine("Сколько вы хотите обменять?");
-                    currencyCount = Convert.ToSingle(Console.ReadLine());
-
-                    if (usd >= currencyCount)
-                    {
-                        usd -= currencyCount;
-                        rub -= currencyCount * usdToRub;
-                    }
-                    break;
-
+                Console.WriteLine("С днём рождения!");
+                age--;
             }
-
-
-            Console.WriteLine("Ваш баланс - " + rub + " рублей и " + usd + " долларов.");
             Console.ReadLine();
         }
     }
