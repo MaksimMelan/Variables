0fe6bd75c782d84f575b117f3b61d04ace531ea1
MaksimMelan <64986385+MaksimMelan@users.noreply.github.com>
2023-02-21 21:42:22 +0500

    Парактика с условными оператовами.

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -6,19 +6,57 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            string password;
-            Console.WriteLine("Здравствуёте! Какой сегодня день недели?");
-            password = Console.ReadLine();
+            int rubToUSD = 72, usdToRub = 75;
+            float rub;
+            float usd;
+            string userInput;
+            float currencyCount;
 
-            if (password == "EDG1fsdR124B")
+            Console.WriteLine("Какую операцию хотите провести?");
+
+            Console.WriteLine("ведите баланс рублей");
+            rub = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("введите баланс долларов");
+            usd = Convert.ToSingle(Console.ReadLine());
+
+            
+            Console.WriteLine("1 -  рубли на доллары.");
+            Console.WriteLine("2 - доллары на рубли.");            
+            userInput = Console.ReadLine();
+            
+            switch (userInput)
             {
-                Console.WriteLine("Проходите!");
-            }
-            else
-            {
-                Console.WriteLine("Вам сюда нельзя");
+                case "1":
+                    Console.WriteLine("1 - обмен  рубли на доллары.");
+                    Console.WriteLine("Сколько вы хотите обменять?");
+                    currencyCount = Convert.ToSingle(Console.ReadLine());
+                    
+                    if (rub >= currencyCount)
+                    {
+                        rub -= currencyCount;
+                        usd += currencyCount / rubToUSD; 
+                    }
+                    else
+                    {
+                        Console.WriteLine("Недопустимое количесвто рублей");
+                    }
+                    break;
+                case "2":
+                    Console.WriteLine("2 - доллары на рубли."); 
+                    Console.WriteLine("Сколько вы хотите обменять?");
+                    currencyCount = Convert.ToSingle(Console.ReadLine());
+
+                    if (usd >= currencyCount)
+                    {
+                        usd -= currencyCount;
+                        rub -= currencyCount * usdToRub;
+                    }
+                    break;
+
             }
 
+
+            Console.WriteLine("Ваш баланс - " + rub + " рублей и " + usd + " долларов.");
             Console.ReadLine();
         }
     }
