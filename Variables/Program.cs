37d76993e93a0d960f1e5ee092143aff5f1cc1fe
MaksimMelan <64986385+MaksimMelan@users.noreply.github.com>
2023-03-09 21:14:45 +0500

    запомнить сумму всех элмементов массива.

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Principal;
+﻿using System;
+using System.Security.Principal;
 
 namespace Variables
 {
@@ -7,14 +8,21 @@ namespace Variables
         static void Main(string[] args)
         {
             // массив int-ов под названием array с небольшим количеством элмементов.
-            int[] array = { 1, 4, 6, 2, 8, 10 };
-
-            for (int i = 0; i < array.Length; i++) // от нуля, до тех пор пока i будет меньше чем array.Length; i увеличиваем на единицу.
-            {
-                Console.WriteLine(array[i]);//в цикле выводим наши элементы поэтапно.
-            }
-
             //запомнить сумму всех элементов массива
+            int[] array = { 1, 4, 6, 2, 8, 10 };
+            int sum = 0;// изначально должна быть равна нулю, для этого нужно
+                        // инициализировать переменную с 0, иначе там даже нуля не будет.
+
+            for (int i = 0; i < array.Length; i++) // от нуля, до тех пор пока i будет
+                                                   // меньше чем array.Length; i увеличиваем на единицу.
+            {
+               sum += array[i];//вывод "Console.WriteLine" убираем, нам нужна только
+                               //ячейка. Мы обращаемся к ячейке и её к себе записываем.
+               Console.WriteLine(sum);//выводим результат каждого сложения для контроля.
+            }
+            Console.WriteLine(sum);
+
+
             Console.ReadKey();
         }
     }
