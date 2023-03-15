a4b543e8bf2ba11283ac68d72f6df3b40e365e2c
MaksimMelan <64986385+MaksimMelan@users.noreply.github.com>
2023-03-15 13:23:11 +0500

    больше меньше.

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -9,25 +9,15 @@ namespace Variables
         static void Main(string[] args)
         {
             int num1 = 0;
-            int num2 = 0;
+            
 
             Console.WriteLine("Ведите первое число: ");
             num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите второе число: ");
-            num2 = Convert.ToInt32(Console.ReadLine());
 
-            if (num1 == num2)
-            {
-                Console.WriteLine("Число " + num1 + " равно числу два " + num2);
-            }
-            else if (num1 > num2)
-            {
-                Console.WriteLine("Первое число " + num1 + " больше чем второе число " + num2);
-            }
+            if (num1 > 5 && num1 < 10)
+                Console.WriteLine("Число больше 5 и меньше 10");
             else
-            {
-                Console.WriteLine("Первое число " + num1 + " меньше второго числа " + num2);
-            }
+                Console.WriteLine("Неизвестное число!");
             Console.ReadKey();
         }
     }
