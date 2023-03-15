6f84980eeb75268bccc59d41e6216a9c23253d4c
MaksimMelan <64986385+MaksimMelan@users.noreply.github.com>
2023-03-15 13:15:38 +0500

    if metanit.

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -8,50 +8,26 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            // система брони столов. мой вариант бронирования case 2.
-            bool isOpen = true;
-            int[] tables = {1, 2, 3, 4, 5, 6, 7, 8, 9 }; // указан не номер стола, а количесвто место за столом.
-            int userInput;
-            int userInput2;
-            // чтоб система не заканчивалась как, кто-то всё забронирует, создадим вечный цикл while
-            while (true) // пока кто-то работает - мы делаем. 
+            int num1 = 0;
+            int num2 = 0;
+
+            Console.WriteLine("Ведите первое число: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 == num2)
             {
-               
-                // вступление
-                Console.WriteLine("1 - вывесит список столов.\n2 - забронировать место.\n3 - выход из программы");
-
-                switch (Convert.ToInt32(Console.ReadLine()))
-                {
-                    case 1:
-                        for (int i = 0; i < tables.Length; i++)//Сначала выведем все столы
-                        {
-                            Console.WriteLine("За столом - " + (i + 1) + " свободно " + tables[i] + " мест.");
-                            // i + 1 делаем для того чтоб нумерация стовлов показывалась как удобно пользователю - не с 0, а с 1.
-                        }
-                        break;
-                    case 2:
-                        Console.WriteLine("Введите номер стола: ");
-                        userInput = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Выбран стол №" + userInput);
-                        Console.WriteLine("Введите количество бронируемых мест: ");
-                        userInput2 = Convert.ToInt32(Console.ReadLine());
-                        tables[(userInput -1)] = userInput2;
-                        Console.WriteLine("Забронирован стол №" + userInput + "\nЗабронированно " + userInput2 + " мест.");
-                        for(int i = 0; i < tables.Length; i++)// выводим столы заного для проверки.
-                        {
-                            Console.WriteLine("За столом - " + (i + 1) + " свободно " + tables[i] + " мест.");
-                            // i + 1 делаем для того чтоб нумерация стовлов показывалась как удобно пользователю - не с 0, а с 1.
-                        }
-                        break;
-                    case 3:
-                        isOpen = false;
-                        break;//мы не можем в таком варианте указать break второй раз - чтоб показать выход из программы
-                              // добавляем isOpen (открыто заведение или закрыто.
-
-                }
-                
+                Console.WriteLine("Число " + num1 + " равно числу два " + num2);
             }
-
+            else if (num1 > num2)
+            {
+                Console.WriteLine("Первое число " + num1 + " больше чем второе число " + num2);
+            }
+            else
+            {
+                Console.WriteLine("Первое число " + num1 + " меньше второго числа " + num2);
+            }
             Console.ReadKey();
         }
     }
