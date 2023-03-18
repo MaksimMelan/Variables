e24e20e9de82d82bd7d3a205a9e6712c749c648b
MaksimMelan <64986385+MaksimMelan@users.noreply.github.com>
2023-03-18 22:42:21 +0500

    Случайный размер массива, и отсеить из всех элементов массива чётные.

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -9,23 +9,33 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            double deposit = 0; 
+            Random random = new Random();
+            int sizeArrey = random.Next(0, 51);
+            int[] cucumbers = new int[sizeArrey]; // размер массива 10, но каждый элемент массива == 0
+            //Console.WriteLine(cucumbers[0]);// обращаемся к первой банке и спрашиваем, что у неё  внутри.
+                                            // мерез имя массива и номер ячейки мы можем обратится к конкертному элементу массива.
+                                            // это похоже на однотипное действие которое повторяется вновь и вновь.
+                                            // значит тут можно использовать массив, так как нам изветсны условия и кол-во то используем for
+
             
+            Console.WriteLine(cucumbers.Length + 1);
+            for (int i = 0; i < cucumbers.Length; i++)
+            {
+                cucumbers[i] = random.Next(0, 101);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < cucumbers.Length; i++)
+            {
+                if(cucumbers[i] % 2 == 0)
+                {
+                    Console.Write(cucumbers[i] + ", ");
+                }
+            }
 
-            Console.WriteLine("Ведите сумму вклада: ");
-             deposit = Convert.ToDouble(Console.ReadLine());
-
-        M:
-            if (deposit < 100)
-                deposit += deposit * 0.05;
-            else if (deposit < 200)
-                deposit += deposit * 0.07;
-            else
-                deposit += deposit * 0.10;
-
-            Console.WriteLine($"Сумма вклада после начисления  равна: {deposit + 15}");
 
             Console.ReadKey();
         }
+            
+        
     }
 }
