d2ea5e4ce5e127165f8df46a97b75a3e707fd4df
MaksimMelan <64986385+MaksimMelan@users.noreply.github.com>
2023-03-29 22:05:15 +0500

    картинки исправление д\з

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -10,14 +10,14 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            int books = 52;
-            int inOneRow = 3;
-            int numberOfRows = books / inOneRow;
-            int remainderOfNumber = books % inOneRow;
-            Console.WriteLine("Количесвто книг: " + books);
-            Console.WriteLine("Сколько картин в ряд: " + inOneRow);
-            Console.WriteLine("Сколько полсностью заполненных рядов: " + numberOfRows);
-            Console.WriteLine("Картинок сверх меры: " + remainderOfNumber);
+            int paintings = 52;
+            int inRowPaintings = 3;
+            int countOfRows = paintings / inRowPaintings;
+            int remainderOfPaintings = paintings % inRowPaintings;
+            Console.WriteLine("Количесвто книг: " + paintings);
+            Console.WriteLine("Сколько картин в ряд: " + inRowPaintings);
+            Console.WriteLine("Сколько полсностью заполненных рядов: " + countOfRows);
+            Console.WriteLine("Картинок сверх меры: " + remainderOfPaintings);
             Console.ReadKey();
         }
     }
