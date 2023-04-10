23f78f916fe35406d9f21e0acdfdf1b3ede1ab83
MaksimMelan <64986385+MaksimMelan@users.noreply.github.com>
2023-04-10 13:04:32 +0500

    очередь старушек 2

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -15,19 +15,19 @@ namespace Variables
             int timeOne = 10;
             int hours = 0;
             int minutes = 0;
+
             while (true)
             { 
                 Console.WriteLine("Здравствуйте!\nВведите число человек в очереди");
                 personQueue = Convert.ToInt32(Console.ReadLine());
+
                 hours = (personQueue * timeOne) / 60;
                 minutes = (personQueue * timeOne) % 60;
+
                 Console.WriteLine($"Время ожидания в очереди составит часов {hours} и {minutes} минут.");
                 Console.ReadKey();
                 Console.Clear();
-            }
-            
-
-            
+            }   
         }
     }
 }
