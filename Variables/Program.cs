a030fad34d53690778915c346557de953e4094ae
MaksimMelan <64986385+MaksimMelan@users.noreply.github.com>
2023-04-11 00:56:29 +0500

    Очередь, исправление 4

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -11,25 +11,22 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            int timeOne = 10;
+            int fixedAppointmentTime = 10;
             int minutesInHours = 60;
 
-            int personQueue = 0;
+            int personQueue;
             int hours = 0;
             int minutes = 0;
 
-            while (true)
-            { 
-                Console.WriteLine("Здравствуйте!\nВведите число человек в очереди");
-                personQueue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Здравствуйте!\nВведите число человек в очереди");
+            personQueue = Convert.ToInt32(Console.ReadLine());
 
-                hours = (personQueue * timeOne) / minutesInHours;
-                minutes = (personQueue * timeOne) % minutesInHours;
+            hours = (personQueue * fixedAppointmentTime) / minutesInHours;
+            minutes = (personQueue * fixedAppointmentTime) % minutesInHours;
 
-                Console.WriteLine($"Время ожидания в очереди составит часов {hours} и {minutes} минут.");
-                Console.ReadKey();
-                Console.Clear();
-            }   
+            Console.WriteLine($"Время ожидания в очереди составит часов {hours} и {minutes} минут.");
+            Console.ReadKey();
+            Console.Clear(); 
         }
     }
 }
